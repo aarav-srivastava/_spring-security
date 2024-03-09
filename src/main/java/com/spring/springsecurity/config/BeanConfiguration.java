2282cd9b0502f6d13cc78b25002d633bfeed827f
Aarav Srivastava <aaravsrivastava2021@gmail.com>
2024-03-09 17:20:34 +0530

    applied authroization rules

--- src/main/java/com/spring/springsecurity/config/BeanConfiguration.java
+++ src/main/java/com/spring/springsecurity/config/BeanConfiguration.java
@@ -7,4 +7,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/spring/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/spring/springsecurity/config/SecurityConfig.java
@@ -1,25 +1,48 @@
 package com.spring.springsecurity.config;
 
-import com.spring.springsecurity.filters.ApiKeyFilter;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
-    @Value("${security-key}")
-    private String key;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.httpBasic(Customizer.withDefaults())
-                .addFilterBefore(new ApiKeyFilter(key), BasicAuthenticationFilter.class)
-                .authorizeHttpRequests(customizer->{
-                    customizer.anyRequest().authenticated();
-                })
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+            return httpSecurity.httpBasic(Customizer.withDefaults())
+                    .authorizeHttpRequests(customizer->{
+                        customizer
+
+                                // authorization rules
+
+//                                .anyRequest().authenticated(); //endpoint level authorization.
+//                                .anyRequest().permitAll()
+                                .anyRequest().hasAuthority("read");
+//                                .anyRequest().hasAnyAuthority("read","write");
+//                                  .anyRequest().hasRole("ADMIN");
+//                                .anyRequest().access("isAuthenticated() and hasAuthority('read')"); //deprecated.
+
+                        // matcher methods
+
+
+                    }) // role is badge whereas authority is action allowed.
+                    .build();
+
+            //customizer.(matcher method + authorization rule)
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){ // using .roles("ADMIN) is same as using .authorities("ROLE_ADMIN") // using ROLE_ as prefix in authorities
+        UserDetails userDetails1 = User.withUsername("aarav").password(passwordEncoder.encode("admin")).authorities("write").roles("SUPER_ADMIN").build(); //role = SUPER_ADMIN
+        UserDetails userDetails2 = User.withUsername("aman").password(passwordEncoder.encode("admin")).authorities("read","ROLE_ADMIN").build(); // role = ADMIN
+        return new InMemoryUserDetailsManager(userDetails1,userDetails2);
     }
 }
